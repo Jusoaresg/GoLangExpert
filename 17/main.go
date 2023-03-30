3717a85236e919b68704f313de94db0d431d2b2e
Juliano Gregorio <julianosgreg@gmail.com>
2023-03-30 20:32:17 -0300

    Ponteiros e Structs

--- 17/main.go
+++ 17/main.go
@@ -2,6 +2,32 @@ package main
 
 //Ponteiros e Structs
 
-func main() {
+type Conta struct {
+	saldo int
+}
 
+/*
+Coisa muito comum (Apenas explicação)
+
+Nessa função "NewConta"
+Estou criando uma função com o endereço da memoria da conta
+para retornar um novo endereço de uma nova conta
+*/
+func NewConta() *Conta {
+	return &Conta{saldo: 0}
+}
+
+//Quando tem o asterisco eu estou mexendo no local da memoria
+func (c *Conta) simular(valor int) int {
+	c.saldo += valor
+	println(c.saldo)
+	return c.saldo
+}
+
+func main() {
+	conta := Conta{
+		saldo: 100,
+	}
+	conta.simular(200)
+	println(conta.saldo)
 }
---
+++ 18/main.go
@@ -0,0 +1,7 @@
+package main
+
+//Interfaces Vazias
+
+func main() {
+
+}
