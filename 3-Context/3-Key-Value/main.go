8ea310eca33604aee6fb30797f99ccb0cc999e61
Juliano Gregorio <julianosgreg@gmail.com>
2023-06-13 12:26:11 -0300

    Adicionado Key-Value

--- 3-Context/3-Key-Value/main.go
+++ 3-Context/3-Key-Value/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main() {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "token", "senha")
+	bookHotel(ctx, "Hotel")
+}
+
+func bookHotel(ctx context.Context, name string) {
+	Token := ctx.Value("token")
+	fmt.Println(Token)
+}
