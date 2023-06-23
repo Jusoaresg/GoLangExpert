9ac4f8389faec7ffafe1e285a93d22633e2bc23f
Juliano Gregorio <julianosgreg@gmail.com>
2023-06-22 23:40:20 -0300

    Adições no GORM

--- 4-Banco-De-Dados/2/main.go
+++ 4-Banco-De-Dados/2/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -24,10 +26,37 @@ func main() {
 		Price: 1000.00,
 	})*/
 
-	products := []Product{
+	// Create Bash
+	/*products := []Product{
 		{Name: "Notebook", Price: 1000.00},
 		{Name: "Mouse", Price: 50.00},
 		{Name: "Keyboard", Price: 100.00},
 	}
-	db.Create(&products)
+	db.Create(&products)*/
+
+	// Select one
+	/*var product Product
+	db.First(&product, 17)
+	db.First(&product, "name = ?", "Mouse")
+	fmt.Println(product)*/
+
+	// Select all
+	/*var products []Product
+	db.Find(&products)
+	for _, product := range products {
+		fmt.Println(product)
+	}*/
+
+	// Where
+	//var products []Product
+	//db.Where("Price > ?", 100).Find(&products)
+	//for _, product := range products {
+	//	fmt.Println(product)
+	//}
+
+	var products []Product
+	db.Where("Name LIKE ?", "%book%").Find(&products)
+	for _, product := range products {
+		fmt.Println(product)
+	}
 }
