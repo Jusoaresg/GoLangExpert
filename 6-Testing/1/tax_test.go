5a184e388b0e8255f349a3d3506eea0dce6e4b44
Jusoaresg <julianosgreg@gmail.com>
2023-07-09 08:30:03 -0300

    feat: testing with batch

--- 6-Testing/1/tax_test.go
+++ 6-Testing/1/tax_test.go
@@ -1,6 +1,8 @@
 package tax
 
-import "testing"
+import (
+	"testing"
+)
 
 /*
 
@@ -18,5 +20,23 @@ func TestCalculateTax(t *testing.T) {
 
 	if result != expected {
 		t.Errorf("Expected %f but got %f", expected, result)
+	}
+}
+
+func TestCalculateTaxBatch(t *testing.T) {
+	type calcTax struct {
+		amount, expect float64
+	}
+
+	table := []calcTax{
+		{500.0, 5.0},
+		{1000.0, 10.0},
+		{1500.0, 10.0},
+	}
+	for _, item := range table {
+		result := CalculateTax(item.amount)
+		if result != item.expect {
+			t.Errorf("Expected %f but got %f", item.expect, result)
+		}
 	}
 }
