c23eb2b5d6c6bb2954f09587ae382e885a4cbfa4
Jusoaresg <julianosgreg@gmail.com>
2023-07-09 08:37:51 -0300

    feat: coverage tests

--- 6-Testing/1/tax_test.go
+++ 6-Testing/1/tax_test.go
@@ -12,6 +12,12 @@ FORMA VERBOSA
 go test . -v
 */
 
+// Faz o cover dos testes, e da uma porcentagem de quanto os testes cobrem o codigo
+//go test -coverprofile=coverage.out
+
+// Abre um html mostrando o lugar no codigo que os testes estão e não estão cobrindo o codigo
+//go tool cover -html=coverage.out
+
 func TestCalculateTax(t *testing.T) {
 	amount := 500.0
 	expected := 5.0
@@ -32,6 +38,7 @@ func TestCalculateTaxBatch(t *testing.T) {
 		{500.0, 5.0},
 		{1000.0, 10.0},
 		{1500.0, 10.0},
+		{0.0, 0.0},
 	}
 	for _, item := range table {
 		result := CalculateTax(item.amount)
